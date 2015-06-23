b2a13585a13ec4ad105a476c1f40fa784729b99f
Rok Z <tugamer@gmail.com>
2015-06-23 16:28:54 +0200

    API services...

--- app/scripts/services/apiService/apiService.js
+++ app/scripts/services/apiService/apiService.js
@@ -6,23 +6,37 @@ define([], function() {
 
         return {
             getInstitutes: getInstitutes
+          //  getGraphs: getGraph
         };
 
         function getInstitutes() {
-
             return $http.get(url + '/institutes')
                     .then(apiSuccess)
                     .catch(apiFail);
 
             function apiSuccess(response) {
-                return response.data.results;
+                return response.data;
             }
 
-            function apiFail(response) {
-                console.log(response.data)
+            function apiFail(error) {
+                console.log("Error: " + error.data)
             }
-
         }
+
+        function getGraph() {
+            return $http.get(url + '/graph')
+                .then(apiSuccess)
+                .catch(apiFail);
+
+            function apiSuccess(response) {
+                return response.data;
+            }
+
+            function apiFail(error) {
+                console.log("Error: " + error.data)
+            }
+        }
+
     }
 
 });
